afbdf393776d6c4c1d8cc0fd9a414516b91b78ee
ngotelli <ngotelli@uvm.edu>
2020-03-18 21:10:45 -0400

    add antcountydata.csv

--- Course_Demos/StructuredProgramming.R
+++ Course_Demos/StructuredProgramming.R
@@ -17,3 +17,62 @@
 # summarize_output()
 # graph_results()
 
+
+# Getting input -----------------------------------------------------------
+
+my_fun <- function(x=5) {
+ z <- 5 + runif(1)
+ return(z)
+}
+my_fun()
+
+# try using m_bar
+########################################
+
+########################################
+
+########################################
+
+# now try using m_sec
+# get data ------------------------------
+
+# load libraries ------------------------------
+
+# try m_head for set up of a new script
+# ----------------------------------
+# Tells what this new script is about
+# 18 Mar 2020
+# NJG
+# ----------------------------------
+#
+
+# try out cool new function snippet
+# ----------------------------------
+# FUNCTION trial_funct
+# description: one line explanation here
+# inputs: just x = 5 for now
+# outputs: real number
+####################################
+trial_funct <- function(x=5) {
+
+# function body
+
+return("Checking...trial_funct")
+
+} # end of trial_funct
+#-----------------------------------
+trial_funct()
+
+# --------------------------------------
+# FUNCTION tester
+# description: description
+# inputs: input_description
+# outputs: output_description
+########################################
+tester <- function(x=5) {
+
+# function body
+
+return("Checking...tester")
+
+} # end of tester
