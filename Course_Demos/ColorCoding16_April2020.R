7e325fccb07a8576dda85f52c0942cfed0bebdbe
ngotelli <ngotelli@uvm.edu>
2020-03-31 18:30:43 -0400

    update color coding script

--- Course_Demos/ColorCoding16_April2020.R
+++ Course_Demos/ColorCoding16_April2020.R
@@ -129,4 +129,64 @@ p_grad + scale_color_gradientn(colors=c("blue",
                                         "purple",
                                         "orange"))
                                       
-                              
+# palette tour ------------------                              
+library(wesanderson)
+print(wes_palettes)
+demoplot(wes_palettes$BottleRocket1,"pie")
+demoplot(wes_palettes[[2]][1:3],"spine")
+
+my_cols <- wes_palettes$GrandBudapest2[1:4]
+p_fil + geom_boxplot() + scale_fill_manual(values=my_cols)
+
+library(RColorBrewer)
+display.brewer.all()
+display.brewer.all(colorblindFriendly = TRUE)
+
+demoplot(brewer.pal(4,"Accent"),"bar")
+demoplot(brewer.pal(11,"Spectral"),"heatmap")
+my_cols <- c("gray75",brewer.pal(3,"Blues"))
+print(my_cols)
+
+p_fil + geom_boxplot() + scale_fill_manual(values=my_cols)
+
+# nice tool for seeing hex values
+library(scales)
+show_col(my_cols)
+
+# viridis palettes
+
+### making a heat map
+x_var <- 1:30
+y_var <- 1:5
+my_data <- expand.grid(x_var=x_var,y_var=y_var)
+head(my_data)
+z_var <- my_data$x_var + my_data$y_var + 2*rnorm(n=150)
+my_data <- cbind(my_data,z_var)
+head(my_data)
+
+# default gradient colors in r
+p4 <- ggplot(my_data,aes(x=x_var,
+                         y=y_var,
+                         fill=z_var)) +
+  geom_tile()
+print(p4)
+
+# user defined divergent palette
+p4 + scale_fill_gradient2(midpoint=19,
+                          low="brown",
+                          mid=gray(0.8),
+                          high="darkblue")
+# use viridis continuous scale
+p4 + scale_fill_viridis_c()
+
+# options viridis, cividis, magma, inferno, plasma
+
+p4 + scale_fill_viridis_c(option="cividis")
+p4 + scale_fill_viridis_c(option="magma")
+p4 + scale_fill_viridis_c(option="inferno")
+p4 + scale_fill_viridis_c(option="plasma")
+
+#desaturation
+p4 <- p4 +scale_fill_viridis_c()
+p4_des <- edit_colors(p4,desaturate)
+plot(p4_des)
